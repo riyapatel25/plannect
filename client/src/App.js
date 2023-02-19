f4a9b0f4c5b9255e0070c1b481679ecf3b0c8926
riyapatel25 <riyap7521@gmail.com>
2023-02-19 02:00:14 -0500

    added mapbox static map

--- client/src/App.js
+++ client/src/App.js
@@ -22,17 +22,22 @@ function App() {
    
     <main>
   <p>Welcome To Plannect</p>
+
     <iframe 
         src="https://jpywnvxt6c4.typeform.com/to/i7hFvACI"
-       > </iframe> 
+       ></iframe> 
      
       <p>
         Similar Interests:
        {user.choices[0].text}
-      </p>  
+      </p>
+      <p>Find People Near You:</p>  
+      <img src="https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/static/-79.467728,43.771912,13,0,60/1200x500?access_token=pk.eyJ1Ijoicml5YXBwMjUiLCJhIjoiY2wyd2s0MnViMG1kbTNsbXZxdmdwd2RiaCJ9.OiS4eMrR0nGsrj_oaKVdmQ"></img>
 
     </main>
   );
 
         }
 export default App;
+
+
