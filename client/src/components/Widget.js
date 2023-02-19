9eae5ddfbd7ed0c4602eef9f31ee8ccfbc2cb738
Cristal Lu <cristallu@eduroam-198-96-32-225.eduroam.yorku.ca>
2023-02-18 20:00:05 -0500

    profile page base

---
+++ client/src/components/Widget.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Widget } from '@typeform/embed-react'
+
+const MyComponent = () => {
+  return <Widget id="<i7hFvACI>" style={{ width: '50%' }} className="my-form" />
+}
+
+
+export default Widget;
